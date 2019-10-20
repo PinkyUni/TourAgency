21c425f439d64a1d094b9bd20801fd47b8d2e23a
PinkyUni <30stmars6277@gmail.com>
2019-10-21 00:01:07 +0300

    specified main logic for user

--- src/com/PinkyUni/model/dao/UserDAO.java
+++ src/com/PinkyUni/model/dao/UserDAO.java
@@ -1,4 +1,7 @@
 package com.PinkyUni.model.dao;
 
 public interface UserDAO {
+    void signIn(String name, String password);
+    void register(String name, String password);
+    void bookTour(int id);
 }
--- src/com/PinkyUni/model/dao/XMLUserDAO.java
+++ src/com/PinkyUni/model/dao/XMLUserDAO.java
@@ -1,4 +1,20 @@
 package com.PinkyUni.model.dao;
 
 public class XMLUserDAO implements UserDAO {
+
+
+    @Override
+    public void signIn(String name, String password) {
+
+    }
+
+    @Override
+    public void register(String name, String password) {
+
+    }
+
+    @Override
+    public void bookTour(int id) {
+
+    }
 }
